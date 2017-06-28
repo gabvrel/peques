7a5d930902ca52742516c65d7d728cdf5cafc221
gabvrel <gabvrel@gmail.com>
2017-06-28 18:19:50 +0300

    Add files via upload

---
+++ js/styles.js
@@ -0,0 +1,12 @@
+$(document)
+    .ready(function () {
+    		$(".abril").click(function() {
+		    	$(".im1").attr("src","images/gallery-2.jpg");
+		    	$(".im2").attr("src","images/gallery-2.jpg"); 
+		    	$(".im3").attr("src","images/gallery-2.jpg");  	
+		    	$(".im4").attr("src","images/gallery-2.jpg"); 
+		    	$(".im5").attr("src","images/gallery-2.jpg"); 
+		    	$(".im6").attr("src","images/gallery-2.jpg"); 
+		    	$(".im7").attr("src","images/gallery-2.jpg"); 
+ 	});                
+});
